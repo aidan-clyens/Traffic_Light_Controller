b1394f02c51cdf351194ad637abd537199fef9e0
Aidan Clyens <aidanc@rogers.com>
2018-05-22 12:13:26 -0400

    Improve formatting

--- cycle_generator.vhd
+++ cycle_generator.vhd
@@ -7,23 +7,23 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY cycle_generator IS
-  PORT (
-    clkin           : in std_logic;
-		rst_n				    : in std_logic;
-		modulo 				  : in integer;		   --	Use different modulo counts to obtain different output frequencies
-		strobe_out		  : out	std_logic;	   --	50% duty cycle clock signal
-		full_cycle_out	: out std_logic	     --	Use as clock enable for the state machine
-  );
+	PORT (
+    		clkin           : in std_logic;
+		rst_n		: in std_logic;
+		modulo 		: in integer;		-- Use different modulo counts to obtain different output frequencies
+		strobe_out	: out std_logic;	-- 50% duty cycle clock signal
+		full_cycle_out	: out std_logic		-- Use as clock enable for the state machine
+  	);
 END cycle_generator;
 
 --------------------------------------------------------------------------------------------------------
 
 ARCHITECTURE counter OF cycle_generator IS
 
-  SIGNAL bin_counter					    : UNSIGNED(31 DOWNTO 0);
-	SIGNAL terminal_count				    : std_logic;
-	SIGNAL half_cycle, full_cycle		: std_logic;
-	SIGNAL strobe							      : std_logic;
+	SIGNAL bin_counter		: UNSIGNED(31 DOWNTO 0);
+	SIGNAL terminal_count		: std_logic;
+	SIGNAL half_cycle, full_cycle	: std_logic;
+	SIGNAL strobe			: std_logic;
 
 	BEGIN
 
@@ -33,78 +33,76 @@ ARCHITECTURE counter OF cycle_generator IS
 
 
 MODULO_COUNTING: PROCESS(clkin, rst_n) IS
-  BEGIN
+	BEGIN
 
-	  IF (rst_n = '0') THEN
-		  bin_counter <= to_unsigned(modulo,32);
-		  terminal_count <= '0';
-      ELSIF (rising_edge(clkin)) THEN
-			-- binary counter decrements on rising clock edge.
+	IF (rst_n = '0') THEN
+		bin_counter <= to_unsigned(modulo,32);
+		terminal_count <= '0';
+      	ELSIF (rising_edge(clkin)) THEN
+		-- binary counter decrements on rising clock edge.
 
-			   IF(bin_counter = 0) THEN
-				     -- when bin_counter reaches 0
-				    -- reload the (converted integer to 32 bit unsigned signal type) modulo value
-				    -- and output a terminal_count signal
-				    bin_counter <= to_unsigned(modulo,32);
-				    terminal_count <= '1';
-			   ELSE
-			      bin_counter <= bin_counter - 1;
-				    terminal_count <= '0';
-			   END IF;
-	  ELSE
-	     bin_counter <= bin_counter;
-		   terminal_count <= terminal_count;
+		IF(bin_counter = 0) THEN
+			-- when bin_counter reaches 0
+			-- reload the (converted integer to 32 bit unsigned signal type) modulo value
+			-- and output a terminal_count signal
+			bin_counter <= to_unsigned(modulo,32);
+			terminal_count <= '1';
+		ELSE
+			bin_counter <= bin_counter - 1;
+			terminal_count <= '0';
+		END IF;
+	ELSE
+		bin_counter <= bin_counter;
+		terminal_count <= terminal_count;
 
-	  END IF;
-  END PROCESS;
+	END IF;
+		
+END PROCESS;
 
 
 Strobe_gen: PROCESS(clkin, rst_n) IS
 	-- Strobe is with 50% duty cycle
+	BEGIN
 
-  BEGIN
+	IF (rst_n = '0') THEN
+		strobe <= '0';
 
-	   IF (rst_n = '0') THEN
-		   strobe <= '0';
+	ELSIF (rising_edge(clkin)) THEN
+		-- Every half cycle, invert the strobe signal
+		IF (half_cycle = '1') THEN
+			strobe <= not strobe;
 
-	   ELSIF (rising_edge(clkin)) THEN
+		ELSE
+			strobe <= strobe;
+		END IF;
 
-		   --	Every half cycle, invert the strobe signal
-		   IF (half_cycle = '1') THEN
-			    strobe <= not strobe;
+	ELSE
+      		strobe <= strobe;
 
-		   ELSE
-			    strobe <= strobe;
-		   END IF;
+	END IF;
 
-	   ELSE
-      strobe <= strobe;
-
-	   END IF;
-
-	END PROCESS;
+END PROCESS;
 
 
 CLKEN_GEN: PROCESS(clkin, rst_n) IS
 	-- full_cycle is one "clkin" cycle in duration and occure once for every two occurrences of half_cycle
+	BEGIN
 
-  BEGIN
+	IF (rst_n = '0') THEN
+      		full_cycle <= '0';
 
-	  IF (rst_n = '0') THEN
-      full_cycle <= '0';
+	ELSIF (rising_edge(clkin)) THEN
 
-	  ELSIF (rising_edge(clkin)) THEN
-
-  	  --	Every second half cycle, set full_cycle to 1 for 20ns, the period of the global 50Hz clock
-      IF (half_cycle = '1' and strobe = '0') THEN
-  		  full_cycle <= '1';
+  		-- Every second half cycle, set full_cycle to 1 for 20ns, the period of the global 50Hz clock
+      		IF (half_cycle = '1' and strobe = '0') THEN
+  			full_cycle <= '1';
   		ELSE
   			full_cycle <= '0';
   		END IF;
 
-	  ELSE
-    END IF;
+	ELSE
+    	END IF;
 
-  END PROCESS;
+END PROCESS;
 
 END ARCHITECTURE;
