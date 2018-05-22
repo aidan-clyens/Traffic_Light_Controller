50049f82ae409f884af0a88f7ad084d8c0ee3608
Aidan Clyens <aidanc@rogers.com>
2018-05-22 10:08:11 -0400

    Update formatting

--- SevenSegmentLights.vhd
+++ SevenSegmentLights.vhd
@@ -1,5 +1,5 @@
 ---------------------------------------------
--- Input Latch
+-- Seven Segment Lights Display Driver
 -- Aidan Clyens
 ---------------------------------------------
 LIBRARY ieee;
@@ -7,19 +7,19 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 -------------------------------------------------------------------------
--- 7-segment dISplay driver. It displays a 4-bit number on a 7-segment
+-- 7-segment display driver. It displays a 4-bit number on a 7-segment
 -- This is created as an entity so that it can be reused many times easily
 
 ENTITY SevenSegmentLights IS
-  PORT (
-    hex	   		    : in std_logic_vector(3 downto 0);   -- The 4 bit data to be dISplayed
-	  strobe5Hz		  :	in std_logic;
-	  strobe1Hz		  :	in std_logic;
-	  night_mode		:	in std_logic;
-	  reduced_mode	: in std_logic;
-	  northsouth		:	out std_logic_vector(6 downto 0);	 --	7-bit outputs to north-south light
-    eastwest 		  : out std_logic_vector(6 downto 0)   -- 7-bit outputs to a east-west light
-  );
+	PORT (
+    		hex		: in std_logic_vector(3 downto 0);	-- The 4 bit data to be displayed
+	  	strobe5Hz	: in std_logic;
+	  	strobe1Hz	: in std_logic;
+	  	night_mode	: in std_logic;
+	  	reduced_mode	: in std_logic;
+	  	northsouth	: out std_logic_vector(6 downto 0);	-- 7-bit outputs to north-south light
+    		eastwest	: out std_logic_vector(6 downto 0)   	-- 7-bit outputs to a east-west light
+  	);
 END SevenSegmentLights;
 
 -----------------------------------------------------------------------------------------------
@@ -35,24 +35,25 @@ BEGIN
 	BEGIN
 
 	IF (reduced_mode = '1') THEN
-
-		IF(strobe1Hz = '1') THEN		--	Flashing amber light, at 1Hz
+		-- Flashing amber light, at 1Hz
+		IF(strobe1Hz = '1') THEN
 			northsouth <= "1000000";
 		ELSE
 			northsouth <= "0000000";
 		END IF;
 
-	elsIF (night_mode = '1') THEN
+	-- Green light
+	ELSIF (night_mode = '1') THEN
 
-		northsouth <= "0001000";		--	Green light
+		northsouth <= "0001000";		
 
 	ELSE
 
 		CASE hex IS
 
 			WHEN "0000" =>
-
-				IF (strobe5Hz = '1') THEN		--	Flashing green light, at 5Hz
+				-- Flashing green light, at 5Hz
+				IF (strobe5Hz = '1') THEN
 					northsouth <= "0001000";
 				ELSE
 					northsouth <= "0000000";
@@ -66,9 +67,10 @@ BEGIN
 					northsouth <= "0000000";
 				END IF;
 
+			-- Green light
 			WHEN "0010" =>
 
-				northsouth <= "0001000";		--	Green light
+				northsouth <= "0001000";	
 
 			WHEN "0011" =>
 
@@ -81,18 +83,20 @@ BEGIN
 			WHEN "0101" =>
 
 				northsouth <= "0001000";
-
+		
+			-- Amber light
 			WHEN "0110" =>
 
-				northsouth <= "1000000";		--	Amber light
+				northsouth <= "1000000";	
 
 			WHEN "0111" =>
 
 				northsouth <= "1000000";
-
+	
+			-- Red light
 			WHEN "1000" =>
 
-				northsouth <= "0000001";		--	Red light
+				northsouth <= "0000001";
 
 			WHEN "1001" =>
 
@@ -133,24 +137,25 @@ BEGIN
 	BEGIN
 
 	IF (reduced_mode = '1') THEN
-
-		IF(strobe1Hz = '1') THEN		--	Flashing red light, at 1Hz
+		-- Flashing red light, at 1Hz
+		IF(strobe1Hz = '1') THEN		
 			eastwest <= "0000001";
 		ELSE
 			eastwest <= "0000000";
 		END IF;
 
-	elsIF (night_mode = '1') THEN
+	-- Red light
+	ELSIF (night_mode = '1') THEN
 
-		eastwest <= "0000001";			--	Red light
+		eastwest <= "0000001";			
 
 	ELSE
 
 		CASE hex IS
-
+			-- Red light
 			WHEN "0000" =>
 
-				eastwest <= "0000001";	--	Red light
+				eastwest <= "0000001";	
 
 			WHEN "0001" =>
 
@@ -180,10 +185,10 @@ BEGIN
 
 				eastwest <= "0000001";
 
-
+			-- Flashing green light, at 5Hz
 			WHEN "1000" =>
 
-				IF (strobe5Hz = '1') THEN	--	Flashing green light, at 5Hz
+				IF (strobe5Hz = '1') THEN	
 					eastwest <= "0001000";
 				ELSE
 					eastwest <= "0000000";
@@ -197,9 +202,10 @@ BEGIN
 					eastwest <= "0000000";
 				END IF;
 
+			-- Green light
 			WHEN "1010" =>
 
-				eastwest <= "0001000";		--	Green light
+				eastwest <= "0001000";		
 
 			WHEN "1011" =>
 				eastwest <= "0001000";
@@ -209,9 +215,10 @@ BEGIN
 
 			WHEN "1101" =>
 				eastwest <= "0001000";
-
+			
+			-- Amber Light
 			WHEN "1110" =>
-				eastwest <= "1000000";		--	Amber Light
+				eastwest <= "1000000";		
 
 			WHEN "1111" =>
 				eastwest <= "1000000";
