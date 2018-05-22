7fd1aea8d6a946884d10db60608cf4d95c77371f
Aidan Clyens <aidanc@rogers.com>
2018-05-22 12:08:38 -0400

    Improve formatting

--- input_latch.vhd
+++ input_latch.vhd
@@ -6,14 +6,15 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
-ENTITY input_latch IS PORT (
-	clk_in			:	in std_logic;		--	Input clock signal
-	rst_n				:	in std_logic;		--	Reset
-	enable			:	in std_logic;		--	Latch enable, comes from the state machine
-	sync_input	:	in std_logic;		--	Synchronized input from the basic_sycnhronizer
-	latch_clear	:	in std_logic;		--	Latch clear, from the state machine
-	data_output	:	out std_logic		--	Latch output, goes to the state machine
-);
+ENTITY input_latch IS
+	PORT (
+		clk_in		: in std_logic;		-- Input clock signal
+		rst_n		: in std_logic;		-- Reset
+		enable		: in std_logic;		-- Latch enable, comes from the state machine
+		sync_input	: in std_logic;		-- Synchronized input from the basic_sycnhronizer
+		latch_clear	: in std_logic;		-- Latch clear, from the state machine
+		data_output	: out std_logic		-- Latch output, goes to the state machine
+	);
 END input_latch;
 
 ------------------------------------------------------------------------------------
@@ -32,15 +33,15 @@ ARCHITECTURE Latch_Design OF input_latch IS
 
 			ELSIF(rising_edge(clk_in)) THEN
 
-				--	Latch only processes data if enable is a 1
+				-- Latch only processes data if enable is a 1
 				IF(enable = '1') THEN
 
-					--	If the synchronized input is a 1, set the output to 1, and hold
+					-- If the synchronized input is a 1, set the output to 1, and hold
 					IF(sync_input = '1') THEN
 
 						data_output <= '1';
 
-					--	Only clear output data when clear is a 1
+					-- Only clear output data when clear is a 1
 					ELSIF(latch_clear = '1') THEN
 
 						data_output <= '0';
